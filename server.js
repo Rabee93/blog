5295624226b7c4a531937d56bfb88940d9a871d2
Rabee Mghrabi <learningprogramming2019@gmail.com>
2020-11-15 21:48:45 +0000

    added article array content and article route

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const articleRouter = require('./routes/articles')
 const app = express()
+
+
+app.set('view engine', 'ejs')
+
+app.use('/articles', articleRouter)
+
 app.get('/', (reg,res) => {
-  res.send('Hello World')
+  const articles = [{
+    title: 'Test Article',
+    createdAt: Date.now(),
+    description: 'Hello there'
+  }]
+  res.render('index', {articles : articles})
 })
 
 app.listen(5000)
